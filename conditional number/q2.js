0002ff141bae48f62229591b06f39ae7ce17bd69
Surya <mrsurajyadavdj@gmail.com>
2025-01-09 17:03:56 +0530

    Add files via upload

---
+++ conditional number/q2.js
@@ -0,0 +1,12 @@
+
+let number = prompt("Enter a number:");
+
+
+number = parseInt(number);
+
+
+if (number % 2 === 0) {
+  console.log("Even Number");
+} else {
+  console.log("The number is not even");
+}
